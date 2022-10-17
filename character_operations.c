82d7b0f33e57ad39ddc6a34b54c05794c0e5183d
Your Name <you@example.com>
2022-10-17 09:19:19 -0700

    Team task

--- character_operations.c
+++ character_operations.c
@@ -1,33 +1,20 @@
 #include "main.h"
 
 /**
- * print_c - Print characters (%c)
- * @ar_list: Store the a list of characters
- * Return: num of parameters printed
+ * print_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
-int print_c(va_list ar_list)
-{
-	int c = va_arg(ar_list, int);
 
-	return (_putchar(c));
-}
-
-/**
-* print_s - print string (%s)
-* @ar_list: Store the a list of characters
-* Return: num of parameters printed
-*/
-int print_s(va_list ar_list)
+int print_string(va_list list)
 {
-	int i, count = 0;
+	int i;
 	char *str;
 
-	str = va_arg(ar_list, char *);
+	str = va_arg(list, char *);
 	if (str == NULL)
 		str = "(null)";
-
-	for (i = 0; str[i]; i++)
-		count += _putchar(str[i]);
-
-	return (count);
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
